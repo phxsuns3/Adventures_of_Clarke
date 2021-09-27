d37656561ebb990b5199415d9d376efa267c16d2
Aaron Baker <acbak338@learner.dvusd.org>
2021-09-27 09:19:54 -0700

    Controls update

--- Adventures_of_Clarke/Assets/moveScript.cs
+++ Adventures_of_Clarke/Assets/moveScript.cs
@@ -18,7 +18,7 @@ public class moveScript : MonoBehaviour
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        if (Input.GetKey(KeyCode.UpArrow) && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
