1ed1eca2363072e8285bcf51fa1e0777966cf1de
Daniel <dscam992@learner.dvusd.org>
2021-09-29 09:24:52 -0700

    Don't pull

---
+++ Adventures_of_Clarke/Assets/Script/CoinCollector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollector : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
