26aea9b041c8ed427cd1c1bbeabf499bcf269812
Deacon <dfbra388@learner.dvusd.org>
2021-11-01 09:16:05 -0700

    Main Menu fixed
    Because daniel broke it

--- Adventures_of_Clarke/Assets/Script/MainMenuController.cs
+++ Adventures_of_Clarke/Assets/Script/MainMenuController.cs
@@ -5,14 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
 
-  public void playGame()
+
+    public void playGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("test");
     }
-   
+
+    public void options()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void back()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
     public void exitGame()
     {
-        Application.Quit();
+        //Application.Quit();
+        
     }
 }
+
