a1a25f2968ff95f89928867e77108fbb4dad9c94
Daniel <dscam992@learner.dvusd.org>
2021-11-03 09:30:51 -0700

    Started working on the enemies

---
+++ Adventures_of_Clarke/Assets/Script/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float m_MaxSpeed;
+
+    [SerializeField]
+    private Vector3[] positions;
+
+    private int index;
+    private Transform target;
+    private float attackCooldown;
+    private bool canAttack = true;
+    private float timeSinceAttack;
+    private int destroy;
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, positions[index], Time.deltaTime
+            * m_MaxSpeed);
+        if(transform.position == positions[index])
+        {
+            if(index == positions.Length -1)
+            {
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+        }
+    }
+    private void Attack()
+    {
+        if(!canAttack)
+        {
+            timeSinceAttack += Time.deltaTime;
+        }
+        if(timeSinceAttack >= attackCooldown)
+        {
+            canAttack = true;
+        }
+        if(canAttack && target != null && Mathf.Abs(target.transform.position.y - transform.position.y) <=1f)
+        {
+            canAttack = false;
+            timeSinceAttack = 0;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (canAttack == true)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                    Destroy(other.gameObject);
+                
+            }
+        }
+    }
+    public void CollisionEnter(string colliderName, GameObject other)
+    {
+        if (colliderName == "Sight" && other.tag == "Player")
+        {
+            if (target == null)
+            {
+                this.target = other.transform;
+            }
+        }
+    }
+}
