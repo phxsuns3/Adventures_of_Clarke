9369f0dd0210d410288263a489ff7f1b2def9259
Daniel <dscam992@learner.dvusd.org>
2021-11-09 09:18:58 -0700

    Pulling an Autumn

---
+++ Adventures_of_Clarke/Assets/Script/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private static int remainingLives = 1;
+    
+   
+
+    // Start is called before the first frame update
+    public GameOver GameOver;
+    int maxPlatform = 0;
+    
+}
